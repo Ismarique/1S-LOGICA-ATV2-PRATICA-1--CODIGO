0a2e1cd8185c88acf70049519d06cda1cc013b19
ismael henrique <ismaelhenrique756@gmail.com>
2025-04-10 14:31:42 -0300

    finalização da atividade 1

---
+++ atvUm/teste.ts
@@ -0,0 +1,12 @@
+let nome: string = `ismael`;
+let idade: number = 16;
+let tempolivreum: string = `jogar jogos online`;
+let tempolivredois: string = `andar de skate`;
+let tempolivretres: string = `praticar esportes`;
+let graduacao: string = `biologia`;
+let graduaçãoDois: string = `microbiologia`;
+
+console.log(`Bom Dia!! Meu nome é ${nome} a minha idade é ${idade}, tenho como momento
+de lazer ${tempolivreum}, ${tempolivredois} e o que mais gosto é ${tempolivreum}. Pretendo ingressar em uma
+universidade, até esse momento estou tendencioso(a) a fazer ${graduacao},
+minha segunda opção é ${graduaçãoDois}.`);
