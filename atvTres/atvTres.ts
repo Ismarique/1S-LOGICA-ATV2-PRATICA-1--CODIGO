487f3018bc201f50e7eade64f7350246bf158087
ismael henrique <ismaelhenrique756@gmail.com>
2025-04-15 14:34:34 -0300

    começo e termino da atividade tres

---
+++ atvTres/atvTres.ts
@@ -0,0 +1,8 @@
+console.log(` Conversor de Temperatura`);
+
+let teclado = require(`prompt-sync`) ();
+let TemperaturaCelsius:number = parseInt(teclado(`Digite o valor da Temperatura que será convertida em Fahrenheit.`)); 
+
+let Fahrenheit: number = (TemperaturaCelsius) * (9/5) + 32; 
+
+console.log(`O valor da temperatura informada em graus Celsius transformada em Fahrenheit é ${Fahrenheit}`); 
