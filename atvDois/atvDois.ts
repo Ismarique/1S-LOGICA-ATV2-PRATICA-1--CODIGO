fcee2b637faa9edb188f60464a9949c25ebc2916
ismael henrique <ismaelhenrique756@gmail.com>
2025-04-15 10:18:42 -0300

    finalisação da atividade 2

---
+++ atvDois/atvDois.ts
@@ -0,0 +1,16 @@
+let teclado = require("prompt-sync")();
+
+let n1: number = parseInt(teclado(`digite um numero: `));
+let n2: number = parseInt(teclado(`digite um numero: `));
+let n3: number = parseInt(teclado(`digite um numero: `));
+let n4: number = parseInt(teclado(`digite um numero: `));
+let n5: number = parseInt(teclado(`digite um numero: `));
+let n6: number = parseInt(teclado(`digite um numero: `));
+let n7: number = parseInt(teclado(`digite um numero: `));
+let n8: number = parseInt(teclado(`digite um numero: `));
+let n9: number = parseInt(teclado(`digite um numero: `));
+let n10: number = parseInt(teclado(`digite um numero: `));
+let media: number = (n1+n2+n3+n4+n5+n6+n7+n8+n9+n10) /10;
+
+console.log (`a media dos numeros é ${media}`);
+
